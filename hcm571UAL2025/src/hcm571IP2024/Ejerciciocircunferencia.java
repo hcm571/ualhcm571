6f329811efccd07d7e23df5b8f8ff5ad8bde7fa9
Castro Morales Hugo Hernán <user@HugoCastroMorales.technicolor.net>
2024-10-12 15:38:18 +0200

    ejercicio entrada circunferencia finalizado

---
+++ hcm571UAL2025/src/hcm571IP2024/Ejerciciocircunferencia.java
@@ -0,0 +1,17 @@
+package hcm571IP2024;
+
+public class Ejerciciocircunferencia {
+
+	public static void main(String[] args) {
+	double radio = 4.5;
+	double perimetro = Math.PI * 2 * radio ;
+	double area = Math.PI * 2 * Math.pow(radio, 2.0);
+	
+	System.out.printf("El perimetro de la circunferencia es igual a "+ perimetro);
+	System.out.println();
+	System.out.println("El área de la circunferencia es "+ area );
+	System.out.println();
+
+	}
+
+}
---
+++ hcm571UAL2025/src/hcm571IP2024/ejerciciocircunferenciaentrada.java
@@ -0,0 +1,25 @@
+package hcm571IP2024;
+
+import java.util.Scanner;
+
+public class ejerciciocircunferenciaentrada {
+
+	public static void main(String[] args) {
+		double radio;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("introduzca el radio de la circunferencia");
+		 radio = teclado.nextDouble();
+		
+		double longitud = 2.0 * Math.PI * radio;
+		double area = Math.PI * Math.pow(radio, 2.0);
+		
+		System.out.println("Radio = " + radio);
+		System.out.println();
+		System.out.printf("Perimetro de la circunferencia = %.3f", longitud);
+		System.out.println();
+		System.out.printf("Area de la circunferencia = %.3f", area);
+		System.out.println();
+		teclado.close();
+	}
+
+}
