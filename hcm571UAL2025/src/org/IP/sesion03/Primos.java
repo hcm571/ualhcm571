3828fa689ba31b7c6609d69c0059b4e9d839806e
Castro Morales Hugo Hernán <user@HugoCastroMorales.technicolor.net>
2024-10-27 10:44:10 +0100

    ejercicio 3 relacion 3 correcto! ;)

--- hcm571UAL2025/src/org/IP/sesion03/Primos.java
+++ hcm571UAL2025/src/org/IP/sesion03/Primos.java
@@ -13,7 +13,7 @@ public class Primos {
 		System.out.println("2. Generar listado de números primos hasta un número dado");
 		System.out.println("3. Salir");
 		System.out.println("Seleccione una opción: ");
-	
+
 
 		programa = entrada.nextInt(); 
 		int x =0; 
@@ -26,60 +26,48 @@ public class Primos {
 			// el programa da varias vueltas, aunque funciona adecuadamente, voy a generar una entrada que a traves de una booleana me diga si la funcion esprimo es verdadera o falsa para dar un resultado mejor. 
 			System.out.println("Escriba un valor de X para calcular si es primo ");
 			x = entrada.nextInt();
-			boolean esprimo = false ;
+			boolean esprimo = true ;
+			if (x<=1) {
+				esprimo=false;
 
-			for (int i=2 ; i< x ; i++) {
-
-				if (x%i == 0) {
-					esprimo = false ;
-					System.out.println("El numero no es primo");
-					break;
-					
-				} else {
-					if (esprimo = true) {
-						System.out.println("El numero " +x +" Es primo");
-						break;
+			}else {
+				for (int i=2;i<x;i++) {
+					if(x%i==0) {
+						esprimo=false;	
 					}
-				}
-				
+				}	
+			}
+			if (esprimo) {
+				System.out.println("el numero "+x+" es primo.");
+			}else {
+				System.out.println("El numero "+x+ " no es primo.");
 			}
 			programa=0;
-		}while (programa ==2 ){
+		}
+		while (programa ==2 ){
 			// Aqui vamos a calcular si todos los numeros primos menores que x y vamos a imprimirlos en forma de lista 
 			// Lo primero es comprobar si el numero es primo o no lo es, para ello, reutilizare el bucle de la opcion 1 
 			System.out.println("introduce el valor de x ");
 			x= entrada.nextInt();
-			boolean esprimo = false ;
 
-			for (int i=2 ; i< x ; i++) {
-
-
-
-				if (x%i == 0) {
-					esprimo = false ;
-					System.out.println("El numero no es primo");
-					break;
-				} }
-			if (esprimo=true) {
-				
 				System.out.println("El listado de los numeros primos menores que  "+x + " son:");
-			for (int j =2 ; j<x ; j++) {
-						boolean esprimomenor = true ;
-						for (int k=2 ; k<j ; k++) {
-							if (j%k == 0) {
-								esprimomenor=false ;
-								break;
-							}
-						}
-						if (esprimomenor) {
-					
+				for (int j =2 ; j<x ; j++) {
+					boolean esprimomenor = true ;
+					 for (int k=2;k<j ; k++) {
+						 if (j%k==0) {
+							 esprimomenor=false;
+							 break;
+						 }
+					 }
+					if (esprimomenor) {
+
 						System.out.print(j +" ");
 						programa=0;
-						}
-							}
-						}
+					}
+				}
 		}
-			while(programa ==3) {
+		
+		while(programa ==3) {
 			System.out.println("Saliendo del programa. ¡Hasta luego!");
 			programa=0;
 		}
